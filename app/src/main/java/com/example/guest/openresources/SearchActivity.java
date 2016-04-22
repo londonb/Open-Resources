2ea382e4d22f16f1d4b60c325bc19b22c4cbee5c
Brad London <london.brad@gmail.com>
2016-04-22 12:35:28 -0700

    add path to comment activity

--- app/src/main/java/com/example/guest/openresources/SearchActivity.java
+++ app/src/main/java/com/example/guest/openresources/SearchActivity.java
@@ -3,6 +3,9 @@ package com.example.guest.openresources;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import butterknife.Bind;
@@ -12,6 +15,8 @@ public class SearchActivity extends AppCompatActivity {
     public static final String TAG = SearchActivity.class.getSimpleName();
 
     @Bind(R.id.userNameGreet) TextView mUserNameGreet;
+    @Bind(R.id.addCommentButton)ImageButton mAddCommentButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,14 @@ public class SearchActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String userName = intent.getStringExtra("userName");
         mUserNameGreet.setText("Hello " + userName + ".");
+
+        mAddCommentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SearchActivity.this, CommentActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
