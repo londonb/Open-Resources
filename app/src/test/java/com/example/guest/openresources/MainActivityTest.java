15ff64e3f7083cc89222c1135b416d0e77de1148
Brad London <london.brad@gmail.com>
2016-04-22 13:04:14 -0700

    pass test for finding text

---
+++ app/src/test/java/com/example/guest/openresources/MainActivityTest.java
@@ -0,0 +1,33 @@
+package com.example.guest.openresources;
+
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by Guest on 4/22/16.
+ */
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent() {
+        TextView textView = (TextView) activity.findViewById(R.id.textView);
+        assertTrue("Enter User Name".equals(textView.getText().toString()));
+    }
+}
