c1185a02926c8f9a48143751c37cae05711c31b6
Brad London <london.brad@gmail.com>
2016-04-29 11:56:17 -0700

    allow user to add comment

--- app/src/main/java/com/example/guest/openresources/SearchActivity.java
+++ app/src/main/java/com/example/guest/openresources/SearchActivity.java
@@ -3,9 +3,11 @@ package com.example.guest.openresources;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,7 +15,9 @@ import android.widget.Toast;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlacePicker;
 
 import butterknife.Bind;
@@ -21,14 +25,15 @@ import butterknife.ButterKnife;
 
 public class SearchActivity extends AppCompatActivity implements View.OnClickListener {
     public static final String TAG = SearchActivity.class.getSimpleName();
+    private String latLong;
+
     private PlacePicker.IntentBuilder builder;
     private static final int PLACE_PICKER_FLAG = 1;
     @Bind(R.id.pickerBtn) Button mPickerBtn;
     @Bind(R.id.myLocation) AutoCompleteTextView mMyLocation;
-
-
+    @Bind(R.id.commentEditText) EditText mCommentEditText;
     @Bind(R.id.userNameGreet) TextView mUserNameGreet;
-//    @Bind(R.id.addCommentButton)ImageButton mAddCommentButton;
+    @Bind(R.id.addCommentButton)Button mAddCommentButton;
 
 
 
@@ -39,9 +44,11 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
         ButterKnife.bind(this);
         builder = new PlacePicker.IntentBuilder();
         mPickerBtn.setOnClickListener(this);
+        GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .build();
 
-
-
+        mAddCommentButton.setOnClickListener(this);
 
         Intent intent = getIntent();
         String userName = intent.getStringExtra("userName");
@@ -52,16 +59,24 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
     }
     @Override
     public void onClick(View v) {
-        try {
-            builder = new PlacePicker.IntentBuilder();
-            Intent intent = builder.build(SearchActivity.this);
-            startActivityForResult(intent, PLACE_PICKER_FLAG);
-        } catch (GooglePlayServicesRepairableException e) {
-            GooglePlayServicesUtil.getErrorDialog(e.getConnectionStatusCode(), SearchActivity.this, 0);
-        } catch (GooglePlayServicesNotAvailableException e) {
-            Toast.makeText(SearchActivity.this, "Google Play Services is not available." ,
-                    Toast.LENGTH_LONG)
-                    .show();
+        if(v == mPickerBtn) {
+            try {
+                builder = new PlacePicker.IntentBuilder();
+                Intent intent = builder.build(SearchActivity.this);
+                startActivityForResult(intent, PLACE_PICKER_FLAG);
+            } catch (GooglePlayServicesRepairableException e) {
+                GooglePlayServicesUtil.getErrorDialog(e.getConnectionStatusCode(), SearchActivity.this, 0);
+            } catch (GooglePlayServicesNotAvailableException e) {
+                Toast.makeText(SearchActivity.this, "Google Play Services is not available.",
+                        Toast.LENGTH_LONG)
+                        .show();
+            }
+        }
+        if(v == mAddCommentButton) {
+           String comment = mCommentEditText.getText().toString();
+
+            Intent intent = new Intent(SearchActivity.this, CommentActivity.class);
+            startActivity(intent);
         }
     }
 
@@ -72,9 +87,14 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
             switch (requestCode) {
                 case PLACE_PICKER_FLAG:
                     Place place = PlacePicker.getPlace(this, intent);
+                    latLong = place.getLatLng().toString();
+                    latLong = latLong.substring(10);
+                    latLong = latLong.substring(0, latLong.length() - 1);
+                    Log.d(TAG, latLong);
                     mMyLocation.setText(place.getName() + ", " + place.getAddress());
                     break;
             }
         }
     }
+
 }
