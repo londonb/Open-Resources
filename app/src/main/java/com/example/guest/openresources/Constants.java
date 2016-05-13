a1501da359d4b66ef23c22647db107171e040f1a
Brad London <london.brad@gmail.com>
2016-05-13 09:04:43 -0700

    add signup activity

--- app/src/main/java/com/example/guest/openresources/Constants.java
+++ app/src/main/java/com/example/guest/openresources/Constants.java
@@ -7,4 +7,9 @@ public class Constants {
     public static final String FIREBASE_URL = BuildConfig.FIREBASE_ROOT_URL;
     public static final String FIREBASE_LOCATIONS = "locations";
     public static final String FIREBASE_URL_LOCATIONS = FIREBASE_URL + "/" + FIREBASE_LOCATIONS;
+
+    public static final String FIREBASE_LOCATION_USERS = "users";
+    public static final String FIREBASE_PROPERTY_EMAIL = "email";
+    public static final String KEY_UID = "UID";
+    public static final String FIREBASE_URL_USERS = FIREBASE_URL + "/" + FIREBASE_LOCATION_USERS;
 }
